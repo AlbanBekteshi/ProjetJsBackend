da3a1a9be3e963f07235616147f9dd542722c234
Zacary Seyed Shafiei <zacary.shafiei@student.vinci.be>
2020-12-07 23:11:38 +0100

    commentaire aps.js

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+/*// Socket.io
+const EXPRESS = require('express');
+let myExpressServerApplication = EXPRESS();
+let myHttpExpressServer = require('http').createServer(myExpressServerApplication);
+
+const io = require("socket.io")(myHttpExpressServer, {
+  cors: {
+    origin: "http://localhost", // Client here is localhost:80
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', socket => {
+  console.log('New Socket Connection');
+  socket.emit("broadcast", "New Socket Client : Welcome !");
+});
+
+myHttpExpressServer.listen(3000, ()  => {
+  console.log('Socket server listening on *:3000');
+});
+*/
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");

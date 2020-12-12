a0044c80bcdcefc8c739a30a98c732e29ed07c13
Zacary Seyed Shafiei <zacary.shafiei@student.vinci.be>
2020-12-12 22:10:49 +0100

    Commentaires

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+/*
+*Cette page à étée réalisée en s'inspirant des tutoriels suivant:
+*Vidéo youtube de David Tang nommé Intro to Web Sockets in Node.js lien: https://www.youtube.com/watch?v=dQTzL3enFng&feature=youtu.be&ab_channel=DavidTang
+*Vidéo youtube de Fireship nommé WebSockets in 100 Seconds & Beyond with Socket.io lien: https://www.youtube.com/watch?v=1BfCnjr_Vjg&ab_channel=Fireship
+*/
+/*
+*Initialisation du webSocketServer
+*/
 const {Server, OPEN} = require("ws");
 const webSocketServer = new Server({ port: 8080 });
 
